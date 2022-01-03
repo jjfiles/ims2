2504161f46c961b038dd5a6dae6a2f9af7c8142e
jjfiles <jeffery@jfiles.dev>
2022-01-03 17:08:50 -0600

    fixed typo

--- chordBuilder.py
+++ chordBuilder.py
@@ -12,24 +12,36 @@ class chordBuilder:
         
         self.system = pyfmodex.System()
         self.system.init(maxchannels=32)
-        self.groups = [] # do i need thi?
         self.master = self.system.master_channel_group
+        self.mainChord = None
         self.volume = 0.5
         self.playing = False
         
         self.base = os.getcwd()
         self.chordPath = os.path.join(self.base, "Chords")
         self.notesPath = os.path.join(self.base, "Notes")
+        self.chordDict = chords
         
+        self.chordFiles = [f for f in os.listdir(self.chordPath) if os.path.isfile(os.path.join(self.chordPath, f))]
+        self.chordFiles.pop(0)
         self.selectedChord = None
-        self.notes = None
-       
+        self.notes = []
+    
     def assign(self):
-        """
-        os.chdir(chords["chord i wanna select"])
-        for quad in chord:
-            
-        """
+
+        # set master chord
+        os.chdir(self.chordPath)
+        self.master = self.system.create_sound(self.chords[self.selectedChord])
+        
+        os.chdir(self.notesPath)
+        c = self.chordFiles[self.selectedChord].split("")[2]
+        self.mainChord = chords[c]
+        
+        for row in self.mainChord:   
+            for note in row:
+                self.notes.append(self.system.ceate_sound(note + ".mp3"))
+        
+        
         pass
     
     def start(self):
